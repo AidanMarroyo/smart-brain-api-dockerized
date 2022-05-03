34e22c6f2fc6c5e7759403ced9aa18aac203db8d
Aidan Manuel-Arroyo <aidanarroyostudent@gmail.com>
2022-05-03 13:47:34 -0400

    added webtoken functionality

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body
   if (!email || !password) {
@@ -25,12 +27,31 @@ const handleSignin = (db, bcrypt, req, res) => {
 const getAuthTokenId = () => {
   console.log('auth ok')
 }
+
+const signToken = (email) => {
+  const jwtPayload = { email }
+
+  //Best practice is to use ENV variable instead of a hard-coded string
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' })
+}
+
+const createSessions = (user) => {
+  // JWT token, return user data
+  const { email, id } = user
+  const token = signToken(email)
+  return { success: 'true', userId: id, token }
+}
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers
   return authorization
     ? getAuthTokenId()
     : handleSignin(db, bcrypt, req, res)
-        .then((data) => res.json(data))
+        .then((data) => {
+          return data.id && data.email
+            ? createSessions(data)
+            : Promise.reject(data)
+        })
+        .then((session) => res.json(session))
         .catch((err) => res.status(400).json(err))
 }
 module.exports = {
